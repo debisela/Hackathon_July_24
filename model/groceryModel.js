21c4d4f661802b886595e075c32c29de9085fbe5
debisela <debora.jaeckel@gmail.com>
2024-08-03 22:37:00 +0300

    fix min letters input

--- model/groceryModel.js
+++ model/groceryModel.js
@@ -3,10 +3,25 @@ const {db} = require('../config/data.js')
 const getCategory = async (itemsArr) =>{
     try {
         let newArr = await Promise.all(itemsArr.map(async (item)=>{
+            if (item.length<3){
+                const result = await db('groceries')
+            .select('item_name','category')
+            .where('item_name', item)
+            return result.length > 0 ? result : { item_name: item, category: 'Category not found' };
+            }
+            else {
             const result = await db('groceries')
             .select('item_name','category')
             .where('item_name', 'ILIKE', `%${item}%`)
-            return result.length > 0 ? result : { item_name: item, category: 'Category not found' };
+            // return result.length > 0 ? result : { item_name: item, category: 'Category not found' };
+            if (result.length == 0){
+                return { item_name: item, category: 'Category not found' }
+            }
+            else if(result.length == 1) {
+                return result
+            }
+            return {item_name: item, category: 'multiple items found, please choose'}
+            }
             
         }))
         return newArr.flat();
